4f550bdd13eb2396694757b7ec3fd07861e5e7f4
gjjun <rlawnstjddl12@gmail.com>
2021-09-01 09:46:02 +0900

    Upload Files
    Files upload

---
+++ LoginForm_NoDB/LoginForm_NoDB/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace LoginForm_NoDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_id = new System.Windows.Forms.TextBox();
+            this.textBox2_pw = new System.Windows.Forms.TextBox();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1_id
+            // 
+            this.textBox1_id.Location = new System.Drawing.Point(12, 47);
+            this.textBox1_id.Multiline = true;
+            this.textBox1_id.Name = "textBox1_id";
+            this.textBox1_id.Size = new System.Drawing.Size(239, 27);
+            this.textBox1_id.TabIndex = 0;
+            // 
+            // textBox2_pw
+            // 
+            this.textBox2_pw.Location = new System.Drawing.Point(12, 128);
+            this.textBox2_pw.Multiline = true;
+            this.textBox2_pw.Name = "textBox2_pw";
+            this.textBox2_pw.Size = new System.Drawing.Size(239, 27);
+            this.textBox2_pw.TabIndex = 1;
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Font = new System.Drawing.Font("굴림", 15F);
+            this.Login_btn.Location = new System.Drawing.Point(12, 173);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(239, 42);
+            this.Login_btn.TabIndex = 2;
+            this.Login_btn.Text = "Login";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            this.Login_btn.Click += new System.EventHandler(this.Login_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 15F);
+            this.label2.Location = new System.Drawing.Point(7, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 15F);
+            this.label3.Location = new System.Drawing.Point(7, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 260);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Login_btn);
+            this.Controls.Add(this.textBox2_pw);
+            this.Controls.Add(this.textBox1_id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_id;
+        private System.Windows.Forms.TextBox textBox2_pw;
+        private System.Windows.Forms.Button Login_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LoginForm_NoDB/LoginForm_NoDB/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm_NoDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_btn_Click(object sender, EventArgs e)
+        {
+            if (textBox1_id.Text == "1234" && textBox2_pw.Text == "1234")
+            {
+                MessageBox.Show("Login Success");
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+                textBox1_id.Clear();
+                textBox2_pw.Clear();
+            }
+        }
+    }
+}
